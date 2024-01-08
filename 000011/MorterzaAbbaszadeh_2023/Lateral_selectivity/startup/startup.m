19020ded5b672e9e314ee23aed57ac32dfcf4522
aranega <vincent.aranega@gmail.com>
2024-01-08 06:23:37 -0600

    DM-57 Add generic function to setup access to dandi data set

--- 000011/MorterzaAbbaszadeh_2023/Lateral_selectivity/startup/startup.m
+++ 000011/MorterzaAbbaszadeh_2023/Lateral_selectivity/startup/startup.m
@@ -1,6 +1,18 @@
-function dataPath = startup(from_location)
-    % Add the helper functions and to the Figures
-    addpath(strcat(fileparts(matlab.desktop.editor.getActiveFilename), filesep, 'Util'));
+function dataPath = startup(from_location, dandiset_id, varargin)
+    persistent p
+    if isempty(p)
+        p = inputParser;
+        addRequired(p, 'from_location', @(x)validateattributes(x, {'string'}, {'nonempty'}))
+        addRequired(p, 'dandiset_id', @(x)validateattributes(x, {'string'}, {'nonempty'}))
+        addParameter(p, 'nwb_path', '', @(x)validateattributes(x, {'string'}, {'nonempty'}))
+        addParameter(p, 'util_libs', [], @(x)isscalar)
+    end
+    parse(p, from_location, dandiset_id, varargin{:});
+    nwb_path = p.Results.nwb_path;
+    util_libs = p.Results.util_libs;
+
+    % Add the helper functions
+    arrayfun(@(x) addpath(strcat(fileparts(matlab.desktop.editor.getActiveFilename), filesep, x)), util_libs)
 
     switch from_location
         case "online"
@@ -12,19 +24,19 @@ function dataPath = startup(from_location)
                       'Enter the Path to the matNWB library'};
             dlgtitle = 'Input';
             dims = [1 35];
-            definput = {'C:\Users\darwinm\Documents\MATLAB\DandiDownload_000011\sub-255201_ses-20141124_behavior+ecephys+ogen.nwb";', ...
+            definput = {'C:\Users\darwinm\Documents\MATLAB\";', ...
                         'C:\Users\darwinm\Documents\MATLAB\matnwb'};
-            answer = inputdlg(prompt,dlgtitle,dims,definput);
+            answer = inputdlg(prompt, dlgtitle, dims, definput);
             dataPath = answer{1};
             matNWBPath = answer{2};
             addpath(genpath(matNWBPath));
             return;
         case "dandi"
             % Download the dataset
-            system('dandi download DANDI:000011/0.220126.1907');
+            system("dandi download DANDI:" + dandiset_id);
 
             % Path to NWB data file(s)
-            dataPath = strcat(pwd, '/000011/sub-255201_ses-20141124_behavior+ecephys+ogen.nwb');
+            dataPath = pwd + "/" + extractBefore(dandiset_id, '/') + "/" + nwb_path;
             return;
     end
 
